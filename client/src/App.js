079a467d52d864d2b410fbefd0cd07fd47125b28
Gene Bertman <gbertman@bertmanlaw.com>
2026-03-13 12:20:31 -0500

    fixed title

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ function App() {
         <Router>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">RacePlaceRC</Link>
+                    <Link className="navbar-brand" to="/">Race Place RC Signup</Link>
                 </div>
             </nav>
             <div className="container mt-4">
